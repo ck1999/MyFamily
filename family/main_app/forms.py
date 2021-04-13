55e6f333efcbfe15218f820a255f87f3c6b56d22
ck1999 <ck199999@rambler.ru>
2021-04-13 14:30:50 +0200

    Forms, Models, Views
    Added some stuff, like
    UserUI, UserUIPos
    Forms is not working now.

---
+++ family/main_app/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django import forms
+from .models import UserUI
+from django.contrib.auth.forms import UserCreationForm
+
+class UserUIAddForm(UserCreationForm()):
+    FIO = forms.CharField(max_length=40, required=True)
+    needUser = forms.BooleanField()
+    pos_x = forms.FloatField()
+    pos_y = forms.FloatField()
+
+    class Meta:
+        model = UserUI
+        fields = (
+            'FIO',
+            'needUser'
+        )
